b921bb7c9ef7b2e910952d6c8dd6fa0f073b703e
Chavdar Angelov <c.l.angelov@gmail.com>
2016-02-08 20:13:45 +0200

    Added Fake Routes  Exceptions

---
+++ VinylC/Web/VinylC.Web.MVC/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿namespace VinylC.Web.MVC.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ErrorController : Controller
+    {
+        public ActionResult My404()
+        {
+            return View();
+        }
+    }
+}

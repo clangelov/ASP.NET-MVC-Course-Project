9cf767e42e023800fd7174733f0c0536e204f7cc
Chavdar Angelov <c.l.angelov@gmail.com>
2016-02-08 21:00:20 +0200

    automapper added

--- VinylC/Web/VinylC.Web.MVC/Global.asax.cs
+++ VinylC/Web/VinylC.Web.MVC/Global.asax.cs
@@ -3,11 +3,13 @@
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
+    using Common.Constants;
 
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            AutoMapperConfig.RegisterMappings(Assemblies.MVCProject);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);

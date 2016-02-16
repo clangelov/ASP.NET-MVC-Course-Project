825c9a165a77ebb19b579913e26137429bb92e52
Chavdar Angelov <c.l.angelov@gmail.com>
2016-02-16 13:21:35 +0200

    Home screen is ready

--- VinylC/Web/VinylC.Web.MVC/Controllers/HomeController.cs
+++ VinylC/Web/VinylC.Web.MVC/Controllers/HomeController.cs
@@ -1,11 +1,18 @@
 ﻿namespace VinylC.Web.MVC.Controllers
 {
+    using System.Linq;
     using System.Web.Mvc;
+    using AutoMapper.QueryableExtensions;
+    using Models.Articles;
+    using Models.Products;
     using Services.Data.Contracts;
     using Services.Web.Contracts;
 
     public class HomeController : Controller
     {
+        private const int ProductsCount = 5;
+        private const int TimeForCache = (5 * 60);
+
         private ICacheService cacheService;
         private IProductService productsService;
         private IArticleService articlesService;
@@ -26,14 +33,30 @@
         [ChildActionOnly]
         public ActionResult GetArticlesPartial()
         {
-            return null;
+            var articlesData =
+                this.cacheService.Get(
+                    "Articles",
+                    () => this.articlesService
+                        .MostCommented(ProductsCount)
+                        .ProjectTo<ArticleSimpleViewModel>(),
+                    TimeForCache);
+
+            return this.PartialView("_SimpleArticlesPartial", articlesData);
         }
 
         [HttpGet]
         [ChildActionOnly]
         public ActionResult GetProductsPartial()
         {
-            return null;
+            var productsData =
+                this.cacheService.Get(
+                    "Products",
+                    () => this.productsService
+                        .GetHighestRated(ProductsCount)
+                        .ProjectTo<ProductSimpleListViewModel>(),
+                    TimeForCache);
+
+            return this.PartialView("_SimpleProductsPartial", productsData);
         }
     }
 }
---
+++ VinylC/Web/VinylC.Web.MVC/Models/Articles/ArticleSimpleViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace VinylC.Web.MVC.Models.Articles
+{
+    using System.Linq;
+    using AutoMapper;
+    using VinylC.Data.Models;
+    using VinylC.Web.MVC.Infrastructure.Mappings;
+
+    public class ArticleSimpleViewModel : IMapFrom<Article>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int CommentsCount { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<Article, ArticleSimpleViewModel>()
+                .ForMember(m => m.CommentsCount, opt => opt.MapFrom(x => x.Comments.Any() ? x.Comments.Count() : 0));
+        }
+    }
+}

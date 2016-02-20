8a2ddea58e26f303a753404cca2b07412ce0993c
Chavdar Angelov <c.l.angelov@gmail.com>
2016-02-20 23:02:18 +0200

    User Controller initial test added

---
+++ VinylC/Tests/VinylC.Tests.Web/Controllers/PrivateArea/UserControllerTests.cs
@@ -0,0 +1,41 @@
+﻿namespace VinylC.Tests.Web.Controllers.PrivateArea
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TestStack.FluentMVCTesting;
+    using VinylC.Services.Data.Contracts;
+    using VinylC.Services.Web;
+    using VinylC.Services.Web.Contracts;
+    using VinylC.Web.MVC.Areas.Private.Controllers;
+
+    [TestClass]
+    public class UserControllerTests
+    {
+        private UserController controller;
+        private IMessageService messageService;
+        private ICacheService cacheService;
+        private IUserService userService;
+
+        [TestInitialize]
+        public void Init()
+        {
+            this.cacheService = new HttpCacheService();
+            this.messageService = ObjectFactory.GetMessageService();
+            this.userService = ObjectFactory.GetUserService();
+            this.controller = new UserController(this.userService, this.messageService, this.cacheService);
+            this.controller.CurrentUser = this.userService.GetUser("test");
+        }
+
+        [TestMethod]
+        public void TestIfIndexPageIsRetuned()
+        {
+            this.controller
+                .WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView();
+        }
+    }
+}

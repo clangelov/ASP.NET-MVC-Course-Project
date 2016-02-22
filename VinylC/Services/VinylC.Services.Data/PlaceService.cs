871de8a9b5befff4dc44fb3894cbc61d09580183
Chavdar Angelov <c.l.angelov@gmail.com>
2016-02-22 11:28:40 +0200

    Service and Controller are added

---
+++ VinylC/Services/VinylC.Services.Data/PlaceService.cs
@@ -0,0 +1,22 @@
+﻿namespace VinylC.Services.Data
+{
+    using System.Linq;
+    using VinylC.Data.Models;
+    using VinylC.Data.Repositories;
+    using VinylC.Services.Data.Contracts;
+
+    public class PlaceService : IPlaceService
+    {
+        private readonly IRepository<Place> places;
+
+        public PlaceService(IRepository<Place> places)
+        {
+            this.places = places;
+        }
+
+        public IQueryable<Place> AllPlaces()
+        {
+            return this.places.All().OrderBy(p => p.Title);
+        }
+    }
+}
---
+++ VinylC/Web/VinylC.Web.MVC/Controllers/PlaceController.cs
@@ -0,0 +1,24 @@
+﻿namespace VinylC.Web.MVC.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using Services.Data.Contracts;
+
+    public class PlaceController : Controller
+    {
+        private IPlaceService placeService;
+
+        public PlaceController(IPlaceService placeService)
+        {
+            this.placeService = placeService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
